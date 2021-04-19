85c412ac1fd31c497a948e87b49904774913855c
nonoka tagami <user@nonopon.local>
2021-04-19 17:15:28 +0900

    タスク２完了

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -33,9 +33,11 @@ public class BooksService {
 
         // TODO 取得したい情報を取得するようにSQLを修正
         List<BookInfo> getedBookList = jdbcTemplate.query(
-                "select * from books order by title asc",
+                "select title,author,publisher,publish_date,id,thumbnail_url from books order by title asc",
 
                 new BookInfoRowMapper());
+
+        //model.addAttribute("bookInfo.title", booksService.getedBookList());
 
         return getedBookList;
     }
