497e1ebeca1b6472bbdfd39dc94a9da1f20ef242
nonoka tagami <user@nonopon.local>
2021-04-22 11:07:21 +0900

    タスク４修正

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -18,6 +18,30 @@ import jp.co.seattle.library.rowMapper.BookInfoRowMapper;
  * 
  *  booksテーブルに関する処理を実装する
  */
+/**
+ * @author user
+ *
+ */
+/**
+ * @author user
+ *
+ */
+/**
+ * @author user
+ *
+ */
+/**
+ * @author user
+ *
+ */
+/**
+ * @author user
+ *
+ */
+/**
+ * @author user
+ *
+ */
 @Service
 public class BooksService {
     final static Logger logger = LoggerFactory.getLogger(BooksService.class);
@@ -65,7 +89,9 @@ public class BooksService {
      *
      * @param bookInfo 書籍情報
      */
+
     public void registBook(BookDetailsInfo bookInfo) {
+
 
         String sql = "INSERT INTO books (title, author,publisher,thumbnail_name,thumbnail_url,reg_date,upd_date) VALUES ('"
                 + bookInfo.getTitle() + "','" + bookInfo.getAuthor() + "','" + bookInfo.getPublisher() + "','"
@@ -77,6 +103,11 @@ public class BooksService {
         jdbcTemplate.update(sql);
     }
 
+    /**
+     * 書籍を削除する
+     *
+     * @param bookId 
+     */
     public void deleteBook(int bookid) {
         String sql = "delete from books where id=" + bookid;
         jdbcTemplate.update(sql);
